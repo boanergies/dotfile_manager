df70ce2a05f3b430ce5dfd5e2df0a88c613ce8e6
Sean McElwain <semcelwa@gmail.com>
2023-08-08 21:37:52 -0500

    added arguments

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+import typer
 
-def main():
+
+def main(dotfile_project_path: str, github_cli: bool = False, https_pull: bool = False):
+    """Command to pull down the specified dotfiles repo and set up
+    the dev environment accordingly. The default is expecting to clone
+    using ssh.
+
+    Args:
+        dotfile_project_path (str): path name given when checking out a gitlab/github project using ssh or gh or https
+        github_cli (bool, optional): indicate if using the github cli. Defaults to False.
+        https_pull (bool, optional): indicate if using https to clone a git project
+    """
     print("Hello world")
 
+
 if __name__ == "__main__":
-    main()
+    typer.run(main)
